bb39c9d4f1b5b6807d9a2b93acb3bbf6c8e659ea
Damian <dkowalski.1997@gmail.com>
2023-07-29 03:16:07 +0200

    moved login logic to controller

--- apps/aurora-backend/src/controllers/auth/auth-controller.ts
+++ apps/aurora-backend/src/controllers/auth/auth-controller.ts
@@ -1,3 +1,8 @@
+import { User } from '../../database/entity/user';
+import bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWTTokens } from '../../interfaces/auth';
+
 export class AuthController {
   private static _controller: AuthController | null = null;
 
@@ -7,5 +12,43 @@ export class AuthController {
     }
 
     return this._controller;
+  }
+
+  public async login(email?: string, password?: string): Promise<JWTTokens> {
+    if (!email || !password) {
+      throw new Error('Email or password was not provided');
+    }
+
+    const user = await this.getUser(email);
+    const compare = await bcrypt.compare(password, user.password);
+    if (!compare) {
+      throw new Error('Password is incorrect');
+    }
+
+    return this.getJwtTokens(email, password);
+  }
+
+  private async getUser(email: string): Promise<User> {
+    const user = await User.findOneBy({ email });
+    if (!user) {
+      throw new Error('No user was found');
+    }
+
+    return user;
+  }
+
+  private getJwtTokens(email: string, password: string): JWTTokens {
+    const accessToken = jwt.sign(
+      { email, password },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '15s' }
+    );
+
+    const refreshToken = jwt.sign(
+      { email, password },
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    return { accessToken, refreshToken };
   }
 }
--- apps/aurora-backend/src/routes/Auth.routes.ts
+++ apps/aurora-backend/src/routes/Auth.routes.ts
@@ -5,42 +5,26 @@ import 'dotenv/config';
 import { authenticateToken } from '../middleware/auth/auth-middleware';
 import { User } from '../database/entity/user';
 import { RefreshToken } from '../database/entity/refresh-token';
+import { AuthController } from '../controllers/auth/auth-controller';
 
 export const router = express.Router();
 
 router.post('/login', async (req, res, next) => {
   const { email, password } = req.body;
+  const controller = AuthController.getInstance();
 
-  if (!email || !password) {
-    throw new Error('ERROR');
+  try {
+    const { accessToken, refreshToken } = await controller.login(
+      email,
+      password
+    );
+    res.cookie('aurora_auth', accessToken);
+    res.cookie('aurora_auth_ref', refreshToken);
+
+    res.sendStatus(200);
+  } catch (err) {
+    return next(err);
   }
-
-  const user = await User.findOneBy({ email });
-
-  if (!user) {
-    return next(new Error('No user was found'));
-  }
-
-  const compare = await bcrypt.compare(password, user.password);
-  if (!compare) {
-    return next(new Error('Password is incorrect'));
-  }
-
-  const accessToken = jwt.sign(
-    { email, password },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: '15s' }
-  );
-
-  const refreshToken = jwt.sign(
-    { email, password },
-    process.env.REFRESH_TOKEN_SECRET
-  );
-
-  res.cookie('aurora_auth', accessToken);
-  res.cookie('aurora_auth_ref', refreshToken);
-
-  res.send(user);
 });
 
 router.post('/register', async (req, res, next) => {
