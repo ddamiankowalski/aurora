b4d4603d70a60e5ae1ae82f5d1a130d4be0f99cd
Damian <dkowalski.1997@gmail.com>
2023-07-29 23:58:12 +0200

    deleted unnecessary components

--- apps/aurora/src/app/shared/auth/auth.module.ts
+++ apps/aurora/src/app/shared/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SideNewsComponent } from './components/side-news/side-news.component';
@@ -8,7 +7,6 @@ import { CommonModule } from '@angular/common';
 import { AuInputModule } from '@aurora/ui';
 import { AuButtonModule } from '../../../../../../libs/ui/src/lib/button/button.module';
 import { AuCheckboxModule } from '../../../../../../libs/ui/src/lib/checkbox/checkbox.module';
-import { RegisterComponent } from './components/register/register.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
 import { AuCardsModule } from '../../../../../../libs/ui/src/lib/cards/cards.module';
@@ -19,8 +17,8 @@ const routes: Routes = [
     path: '',
     component: AuthComponent,
     children: [
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginFormComponent },
+      { path: 'register', component: RegisterFormComponent },
       { path: '**', redirectTo: 'login' },
     ],
   },
@@ -45,8 +43,6 @@ const routes: Routes = [
   declarations: [
     LoginFormComponent,
     SideNewsComponent,
-    LoginComponent,
-    RegisterComponent,
     AuthComponent,
     RegisterFormComponent,
   ],
